66e34dccec58fc891f04781b372ba3468ca5316f
Jeffrey Gunderson <jeffrey.gunderson@gmail.com>
2014-07-19 08:31:11 -0600

    Dynamically added questions and answers work

--- app/helpers/quizzes_helper.rb
+++ app/helpers/quizzes_helper.rb
@@ -9,11 +9,21 @@ module QuizzesHelper
 		link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
 	end
 
-	# def link_to_add_questions(f)
-	# 	new_object = f.object.sent(:questions).class.new
+	def link_to_add_questions(f)
+		new_question = f.object.send(:questions).klass.new
+		id = new_question.object_id
+		new_question.build_answer
+		fields = f.fields_for(:questions, new_question, child_index: id) do |builder|
+			render("question_fields", f: builder)
+		end
+		link_to("Add Question", '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+	end
+	# def add_answer(f)
+	# 	# binding.pry
+	# 	new_object = f.object.send(:answer).class.new
 	# 	id = new_object.object_id
-	# 	fields = f.fields_for(association, new_object, child_index: id) do |builder|
-	# 		render(question)
-		
-		
+	# 	fields = f.fields_for(:answer, new_object, child_index:id) do |builder|
+	# 		render "answer_fields", f: builder
+	# 	end
+	# end
 end
