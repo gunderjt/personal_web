d4e56d1c13a3775afa92e59b05321970c5c13275
Jeffrey Gunderson <jeffrey.gunderson@gmail.com>
2014-09-12 16:06:00 -0400

    changed Database, hope this doesn't screw anything up... :/

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140713180634) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: true do |t|
     t.integer  "question_id"
     t.text     "content"
@@ -49,7 +52,7 @@ ActiveRecord::Schema.define(version: 20140713180634) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
